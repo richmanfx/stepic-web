badccdd053009435a2eaae4cb4e00ef75d5a47ac
Aleksandr Jashhuk <richmanfx@yandex.ru>
2016-03-09 18:41:22 +0300

    Добавлены две модели и нстроена работа с MySQL - syncdb отработал.

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -56,10 +56,22 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ask_database',
+        'USER': 'root',
+        'PASSWORD': '2468',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, )
+    likes = [User, ]
+
+    def __unicode__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now=True)
+    question = Question
+    author = models.ForeignKey(User, )
+
+    def __unicode__(self):
+        return self.title
