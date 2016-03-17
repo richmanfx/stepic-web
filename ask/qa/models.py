86beb388329a98a21f883c66696292d2ebfb1f3a
Aleksandr Jashhuk <richmanfx@yandex.ru>
2016-03-17 19:16:10 +0300

    мучаю модели, вьюшки...

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class Question(models.Model):
@@ -15,14 +16,18 @@ class Question(models.Model):
     def __unicode__(self):
         return self.title
 
+    def get_url(self):
+        return reverse('question', kwargs={'id': self.id})
+
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    # question = models.OneToOneField(Question)
-    # question = models.ForeignKey(Question, related_name='answer_set') 
-    question = models.ForeignKey(Question, related_name='a_question') 
+    question = models.ForeignKey(Question, related_name='answer_set') 
     author = models.ForeignKey(User, related_name='a_authon', null=True)
 
     def __unicode__(self):
         return self.text
+
+    def get_url(self):
+        return reverse()
