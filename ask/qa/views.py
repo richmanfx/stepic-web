359b3bc26b2bc3964ee878f59f03d7ad26c6523d
Aleksandr Jashhuk <richmanfx@yandex.ru>
2016-03-17 18:53:47 +0300

    мучаю модели, вьюшки...

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -17,9 +17,13 @@ def test(request, *args, **kwargs):
 def main_page(request):
     # return HttpResponse('Отработала "main_page".')
 
-    questions = Question.objects.all()		# получить все вопросы    
-    questions = questions.order_by('-id')	# последний заданный вопрос - первый в списке
-    
+    all_questions = Question.objects.all()		# получить все вопросы    
+    #question = Question.objects.get(id=0)
+    all_questions = all_questions.order_by('-id')	# последний заданный вопрос - первый в списке
+    questions = all_questions[0:5]			# срез - первые 6 вопросов
+    #qa = Question.objects.create(title='First question')
+    #qa.text='eto pervy vopros.'
+    #qa.save()
+
 
     return render(request, 'main_page.html', {'questions':	questions})
-
